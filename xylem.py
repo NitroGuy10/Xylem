7961b47d0e7fa0fb96230b359ae42f7c25fcf119
NitroGuy10 <naviyer10@gmail.com>
2023-02-05 00:48:06 -0600

    Now fully customizable!

--- xylem.py
+++ xylem.py
@@ -29,26 +29,11 @@ import argparse
 import pathlib
 import subprocess
 import os
-
-
-
-def run_convert_subprocess(input_path: pathlib.Path, output_path: pathlib.Path, desired_file_format: str) -> None:
-    process = None
-    if desired_file_format in ["jpg", "jpeg", "png", "tif", "tiff", "bmp", "webp", "heif", "heic"]:
-        print(f"Converting {input_path.name} to {desired_file_format} with ImageMagick")
-        process = subprocess.run(["magick", str(input_path), str(output_path)])
-        print("Conversion finished")
-    else:
-        print(f"Converting {input_path} to {desired_file_format} with FFmpeg")
-        process = subprocess.run(["ffmpeg", "-y", "-i", str(input_path), str(output_path)])  # -y for force overwriting
-        print("Conversion finished")
-    
-    # Halt the program and display an error message if a conversion subprocess fails
-    if process.returncode != 0:
-        raise RuntimeError(f"Conversion subprocess for {input_path} failed with return code {process.returncode}")
+import json
 
 
 if __name__ == "__main__":
+    # Argparse stuff
     parser = argparse.ArgumentParser()
     parser.add_argument("input_files_folders", nargs="*", help="The input file(s) and/or folder(s) to convert")  # This takes in drag-n-drop args too!!!
     parser.add_argument("-f", "--file_format", help="The format you want to convert to (will prompt if not specified)")
@@ -61,7 +46,6 @@ if __name__ == "__main__":
     file_format = args.file_format
     input_files_folders = args.input_files_folders
     output_folder = args.output_folder
-
 
     if len(args.input_files_folders) == 0:  # No CLI arguments provided
         # Prompt for input files/folders
@@ -132,10 +116,56 @@ if __name__ == "__main__":
         else:
             data_tuple = (input_path_str, "")
             input_data.append(data_tuple)
+    
+    # Load config.json
+    config_path = pathlib.Path(__file__).parent.joinpath("config.json")
+    config = None
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+    
+    # Identify the appropriate converter to use
+    file_format = file_format.lower()
+    converter = None
+    for converter_config in config["converters"]:
+        if file_format in converter_config["formats"]:
+            converter = converter_config
+            break
+    
+    if converter is None:
+        # Prompt the user if a converter isn't indentified
+        print(f"A converter could not be automatically identified for .{file_format} files")
+        print(f"To avoid this prompt in the future, modify config.json")
+        print("Converters available:")
+        for converter_index in range(len(config["converters"])):
+            print(f"{converter_index + 1}.   {config['converters'][converter_index]}")
+        desired_converter_index = int(input("Input the number corresponding with the converter to use: "))
+        converter = config["converters"][desired_converter_index]
+    else:
+        # Prompt the user to specify formatArgs
+        if file_format in config["formatArgs"]:
+            for format_arg in config["formatArgs"][file_format]:
+                arg_input = input(format_arg["prompt"]).strip()
+                if format_arg["inputType"] == "string":
+                    if len(arg_input) == 0:  # If an empty string was given
+                        for added_argument_index in range(len(format_arg["argumentsIfEmpty"])):
+                            converter["runCommand"].insert(format_arg["argumentsBeforeIndex"] + added_argument_index, format_arg["argumentsIfEmpty"][added_argument_index])
+                    else:
+                        for added_argument_index in range(len(format_arg["argumentsIfNotEmpty"])):
+                            converter["runCommand"].insert(format_arg["argumentsBeforeIndex"] + added_argument_index, format_arg["argumentsIfNotEmpty"][added_argument_index].replace("$$STRING$$", arg_input))
+                elif format_arg["inputType"] == "y/n":
+                    if arg_input.lower() == "n":
+                        for added_argument_index in range(len(format_arg["argumentsIfNo"])):
+                            converter["runCommand"].insert(format_arg["argumentsBeforeIndex"] + added_argument_index, format_arg["argumentsIfNo"][added_argument_index])
+                    elif arg_input.lower() == "y":
+                        for added_argument_index in range(len(format_arg["argumentsIfYes"])):
+                            converter["runCommand"].insert(format_arg["argumentsBeforeIndex"] + added_argument_index, format_arg["argumentsIfYes"][added_argument_index])
+                    else:
+                        raise ValueError("Must input \"y\" or \"n")
+                else:
+                    raise TypeError(f"Unknown inputType {format_arg['input_type']}")
 
 
     # Main conversion loop
-    file_format = file_format.lower()
     for input_path_str, output_folder_additional_path in input_data:
         input_path = pathlib.Path(input_path_str)
         # Check if the input file exists... again... just to make sure I didn't screw anything up
@@ -171,7 +201,16 @@ if __name__ == "__main__":
 
         # At this point, conversion would either not overwrite anything or would overwrite because the user allowed it
         # Convert!
-        run_convert_subprocess(input_path, output_path, file_format)
+        command_sequence = [command_item.replace("$$INPUT_PATH$$", str(input_path)).replace("$$OUTPUT_PATH$$", str(output_path)) for command_item in converter["runCommand"]]
+
+        print(f"Converting {input_path.name} to {file_format} with {converter['name']}")
+        print(" ".join(command_sequence))
+        process = subprocess.run(command_sequence)
+        print("Conversion finished")
+        
+        # Halt the program and display an error message if a conversion subprocess fails
+        if process.returncode != 0:
+            raise RuntimeError(f"Conversion subprocess for {input_path} failed with return code {process.returncode}")
 
     print("All done!")
 
